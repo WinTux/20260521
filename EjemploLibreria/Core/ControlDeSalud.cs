b4ff4c501d99f765a73cd2f542f5c9d88e2a56c0
rusok <rusok.software@gmail.com>
2026-05-21 17:37:29 -0400

    Agregar archivos de proyecto.

---
+++ EjemploLibreria/Core/ControlDeSalud.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace juego
+{
+    namespace player
+    {
+        public class ControlDeSalud
+        {
+            public int SaludMaxima { get; private set; }
+            public int SaludActual { get; private set; }
+            public bool EstaMuerto => SaludActual <= 0;
+
+            public ControlDeSalud(int saludMaxima)
+            {
+                SaludMaxima = saludMaxima;
+                SaludActual = saludMaxima;
+            }
+            public void RecibirDanyo(int danyo)
+            {
+                if (EstaMuerto)
+                    return;
+                SaludActual -= danyo;
+                if (SaludActual < 0)
+                    SaludActual = 0;
+            }
+            public void Curar(int cantidad)
+            {
+                if (EstaMuerto)
+                    return;
+                SaludActual += cantidad;
+                if (SaludActual > SaludMaxima)
+                    SaludActual = SaludMaxima;
+            }
+            public void ReiniciarSalud()
+            {
+                SaludActual = SaludMaxima;
+            }
+        }
+    }
+    namespace enemigo {
+        public class ControlDeSalud
+        {
+            public int SaludMaxima { get; private set; }
+            public int SaludActual { get; private set; }
+            public bool EstaMuerto => SaludActual <= 0;
+
+            public ControlDeSalud(int saludMaxima)
+            {
+                SaludMaxima = saludMaxima;
+                SaludActual = saludMaxima;
+            }
+            public void RecibirDanyo(int danyo)
+            {
+                if (EstaMuerto)
+                    return;
+                SaludActual -= danyo;
+                if (SaludActual < 0)
+                    SaludActual = 0;
+            }
+            public void Curar(int cantidad)
+            {
+                if (EstaMuerto)
+                    return;
+                SaludActual += cantidad;
+                if (SaludActual > SaludMaxima)
+                    SaludActual = SaludMaxima;
+            }
+            public void ReiniciarSalud()
+            {
+                SaludActual = SaludMaxima;
+            }
+        }
+    }
+    public class Controlenemigo { 
+    }
+}
---
+++ EjemploLibreria/Core/HerramientaAdicional.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+namespace EjemploLibreria.Core
+{
+    using juego.player;
+    using ControlDeSaludEnemy = juego.enemigo.ControlDeSalud;
+    internal class HerramientaAdicional
+    {
+        ControlDeSalud controlcitoJugador = new ControlDeSalud(100);
+        ControlDeSaludEnemy superControlenemigo = new ControlDeSaludEnemy(1000);
+    }
+}
+namespace juego.player {
+    public class HerramientaAdicional2
+    {
+        ControlDeSalud controlcitoJugador = new ControlDeSalud(100);
+    }
+}
